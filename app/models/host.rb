23df33f2ecb2d38adad698a05232914955d18610
Jacek Podkanski <jacek.podkanski@clarussoftware.co.uk>
2016-10-05 13:35:12 +0100

    added basic associations

--- app/models/host.rb
+++ app/models/host.rb
@@ -1,3 +1,7 @@
+# it's a role of the user
 class Host < ApplicationRecord
+  belonngs_to :user
+  has_many :visitors
+
   validates :name, :location, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_one :host
+  has_many :visitors, through: :host
 end
